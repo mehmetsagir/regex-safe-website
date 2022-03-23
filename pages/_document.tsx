33ed7b65968c4fc62a67931ed846b3fae216a13c
mehmetsagir <memetsagirint@gmail.com>
2022-03-23 21:58:35 +0300

    update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,20 @@
-import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+import { createStylesServer, ServerStyles } from "@mantine/next";
+import Document, { DocumentContext } from "next/document";
 
-export default class Document extends NextDocument {
-  // eslint-disable-next-line class-methods-use-this
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+const stylesServer = createStylesServer();
+
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <ServerStyles html={initialProps.html} server={stylesServer} />
+        </>
+      ),
+    };
   }
 }
