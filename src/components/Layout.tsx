a36352544987f8616a0517766532fa4cc88e6dcb
mehmetsagir <memetsagirint@gmail.com>
2022-04-10 17:53:13 +0300

    add Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,26 @@
+import styled from "styled-components";
+
 import Header from "./Header";
+import Menu from "./Menu";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <>
+    <Container>
       <Header />
-      {children}
-    </>
+      <main>
+        <Menu />
+        {children}
+      </main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  max-height: 100vh;
+  main {
+    display: flex;
+    height: 100%;
+  }
+`;
 
 export default Layout;
