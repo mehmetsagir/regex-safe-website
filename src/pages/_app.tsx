577104328dcdb7809fb17c5707fd1f1149c2af76
mehmetsagir <memetsagirint@gmail.com>
2022-04-11 03:32:26 +0300

    change type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
-import type { AppProps } from "next/app";
 import Layout from "src/components/Layout";
 import theme from "src/styles/theme";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "../styles/GlobalStyles";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
