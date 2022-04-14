f482f4880eab639d3a6e592c0e37f78883a84dd8
mehmetsagir <memetsagirint@gmail.com>
2022-04-14 03:23:12 +0300

    add colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,6 +6,8 @@ const theme = {
     primary: lighten(0.03, defaultColor),
     secondary: lighten(0.1, defaultColor),
     text: lighten(0.7, defaultColor),
+    textSecondary: lighten(0.4, defaultColor),
+    success: "#28a745",
     link: "#00aaff",
   },
 };
