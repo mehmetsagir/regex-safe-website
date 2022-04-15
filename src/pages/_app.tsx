c74ec58af44258439dc41d50ebe267a2105bd4a9
mehmetsagir <memetsagirint@gmail.com>
2022-04-15 03:30:45 +0300

    add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Layout from "src/components/Layout";
 
 function MyApp({ Component, pageProps }: any) {
   return (
     <Layout>
+      <Head>
+        <title>Regex Safe</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
