0fe73d1ba40bab1cdc309f924576a3cdf1a7a98a
mehmetsagir <memetsagirint@gmail.com>
2022-04-17 03:23:22 +0300

    add loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Layout from "src/components/Layout";
+import Loader from "src/components/Loader";
 
 function MyApp({ Component, pageProps }: any) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 250);
+  }, [router.pathname]);
+
   return (
     <Layout>
       <Head>
         <title>Regex Safe</title>
       </Head>
+      {loading && <Loader />}
       <Component {...pageProps} />
     </Layout>
   );
