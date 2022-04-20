4eef791f486fbb6aef131a4f843685f7a619ec9d
mehmetsagir <memetsagirint@gmail.com>
2022-04-20 05:57:30 +0300

    fix vercel build error

--- src/pages/controls/email.tsx
+++ src/pages/controls/email.tsx
@@ -1,18 +1,20 @@
 import PageContent from "src/components/PageContent";
 
-const Email = () => (
-  <PageContent
-    title="Email"
-    placeholder="Enter an email"
-    code={{
-      el: "isEmail",
-      regex: "/^[w-.]+@([w-]+.)+[w-]{2,4}$/g",
-      value: "email",
-    }}
-    exampleCode={
-      "import { isEmail } from 'regex-safe'; \n\nisEmail('mehmetsagirdev@gmail.com'); // 'true'"
-    }
-  />
-);
+const Email = () => {
+  return (
+    <PageContent
+      title="Email"
+      placeholder="Enter an email"
+      code={{
+        el: "isEmail",
+        regex: "/^[w-.]+@([w-]+.)+[w-]{2,4}$/g",
+        value: "email",
+      }}
+      exampleCode={
+        "import { isEmail } from 'regex-safe'; \n\nisEmail('mehmetsagirdev@gmail.com'); // 'true'"
+      }
+    />
+  );
+};
 
 export default Email;
