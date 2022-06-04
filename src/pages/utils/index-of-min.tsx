849c037a59e8ae130e194f398f8151e4967a05a9
mehmetsagir <memetsagirint@gmail.com>
2022-06-04 21:27:42 +0300

    add indexOfMin

---
+++ src/pages/utils/index-of-min.tsx
@@ -0,0 +1,45 @@
+// @ts-nocheck
+import { indexOfMin } from "regex-safe";
+import Container from "src/components/Container";
+import Editor from "src/components/Editor";
+
+const MaxByArray = () => {
+  const values = [7, 3, 6, 10, 23];
+
+  return (
+    <Container title="Index of Min">
+      <h1>indexOfMin</h1>
+      {<p className="desc">Find the index of the minimum item of an array</p>}
+      <h4>Code</h4>
+      <Editor
+        style={{
+          marginTop: 12,
+        }}
+        language="jsx"
+        code={
+          "const indexOfMin = (arr) => arr.reduce((prev, curr, i, a) => (curr < a[prev] ? i : prev), 0);"
+        }
+      />
+      <div className="example">
+        <div>
+          <h4>Example Value</h4>
+          <Editor
+            style={{
+              marginTop: 12,
+            }}
+            language="jsx"
+            code={"indexOfMin([7, 3, 6, 10, 23])"}
+          />
+        </div>
+        {!!indexOfMin(values) && (
+          <div className="output">
+            <h5>Output</h5>
+            <span>{indexOfMin(values)}</span>
+          </div>
+        )}
+      </div>
+    </Container>
+  );
+};
+
+export default MaxByArray;
