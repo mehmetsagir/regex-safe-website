d6b88b7c7371e6d51679a6b507d6530545f20e25
mehmetsagir <memetsagirint@gmail.com>
2022-07-01 00:06:24 +0300

    add monthDiff

---
+++ src/pages/utils/month-diff.tsx
@@ -0,0 +1,37 @@
+import Container from "src/components/Container";
+import Editor from "src/components/Editor";
+
+const DiffDays = () => {
+  return (
+    <Container title="Month Diff">
+      <h1>monthDiff</h1>
+      <p className="desc">Calculate the number of months between two dates</p>
+      <h4>Code</h4>
+      <Editor
+        style={{
+          marginTop: 12,
+        }}
+        language="jsx"
+        code={
+          "const monthDiff = (startDate, endDate) => Math.max(0, (endDate.getFullYear() - startDate.getFullYear()) * 12 - startDate.getMonth() + endDate.getMonth());"
+        }
+      />
+      <div className="example">
+        <div>
+          <h4>Example Usage</h4>
+          <Editor
+            style={{
+              marginTop: 12,
+            }}
+            language="jsx"
+            code={
+              "monthDiff(new Date('2020-01-01'), new Date('2021-01-01')); // 12"
+            }
+          />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default DiffDays;
