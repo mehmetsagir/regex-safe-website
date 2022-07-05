2cbcf0e5f43b68b462c3ce64761c4781985a39e0
mehmetsagir <memetsagirint@gmail.com>
2022-07-05 22:28:54 +0300

    add dayOfYear

---
+++ src/pages/utils/day-of-year.tsx
@@ -0,0 +1,35 @@
+import Container from "src/components/Container";
+import Editor from "src/components/Editor";
+
+const DayOfYear = () => {
+  return (
+    <Container title="dayOfYear">
+      <h1>dayOfYear</h1>
+      <p className="desc">Group an array of objects by a key</p>
+      <h4>Code</h4>
+      <Editor
+        style={{
+          marginTop: 12,
+        }}
+        language="jsx"
+        code={
+          "const dayOfYear = (date) => Math.floor((date - new Date(date.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24));"
+        }
+      />
+      <div className="example">
+        <div>
+          <h4>Example Usage</h4>
+          <Editor
+            style={{
+              marginTop: 12,
+            }}
+            language="jsx"
+            code={"dayOfYear(new Date(2020, 04, 16)); // 137"}
+          />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default DayOfYear;
