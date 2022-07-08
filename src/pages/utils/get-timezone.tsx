e775de4cdad59983acf39ad55ffd191dd508be44
mehmetsagir <memetsagirint@gmail.com>
2022-07-08 20:58:21 +0300

    add getTimezone

---
+++ src/pages/utils/get-timezone.tsx
@@ -0,0 +1,47 @@
+import Container from "src/components/Container";
+import Editor from "src/components/Editor";
+
+// @ts-ignore
+import { getTimezone } from "regex-safe";
+
+const GetTimezone = () => {
+  return (
+    <Container title="Get Timezone">
+      <h1>getTimezone</h1>
+      <p className="desc">Get the timezone string</p>
+      <h4>Code</h4>
+      <Editor
+        style={{
+          marginTop: 12,
+        }}
+        language="jsx"
+        code={
+          "const getTimezone = () => Intl.DateTimeFormat().resolvedOptions().timeZone;"
+        }
+      />
+      <div className="example">
+        <div>
+          <h4>Example Usage</h4>
+          <Editor
+            style={{
+              marginTop: 12,
+            }}
+            language="jsx"
+            code={"getTimezone(); // Europe/Istanbul"}
+          />
+          <p
+            style={{
+              marginTop: 24,
+              color: "#ccc",
+              opacity: 0.6,
+            }}
+          >
+            Current: {getTimezone()}
+          </p>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default GetTimezone;
